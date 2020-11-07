c2a3bda944ec09d779c9750d8c92d6116b36f64f
Tom Sullivan <tom@msbit.com.au>
2020-11-08 00:12:10 +1100

    Formatting via `swiftformat`

--- au-naturel.swift
+++ au-naturel.swift
@@ -3,19 +3,19 @@ import Darwin
 
 var signalSources: [Int32: DispatchSourceSignal] = [:]
 
-func reverseDeltas(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent, refcon: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
-  let isContinuous = event.getIntegerValueField(.scrollWheelEventIsContinuous)
-  if isContinuous == 1 { return Unmanaged.passUnretained(event) }
+func reverseDeltas(_: CGEventTapProxy, _: CGEventType, event: CGEvent, _: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
+    let isContinuous = event.getIntegerValueField(.scrollWheelEventIsContinuous)
+    if isContinuous == 1 { return Unmanaged.passUnretained(event) }
 
-  let pointDelta = event.getIntegerValueField(.scrollWheelEventPointDeltaAxis1)
-  let delta = event.getIntegerValueField(.scrollWheelEventDeltaAxis1)
-  let fixedPtDelta = event.getIntegerValueField(.scrollWheelEventFixedPtDeltaAxis1)
+    let pointDelta = event.getIntegerValueField(.scrollWheelEventPointDeltaAxis1)
+    let delta = event.getIntegerValueField(.scrollWheelEventDeltaAxis1)
+    let fixedPtDelta = event.getIntegerValueField(.scrollWheelEventFixedPtDeltaAxis1)
 
-  event.setIntegerValueField(.scrollWheelEventPointDeltaAxis1, value: -pointDelta)
-  event.setIntegerValueField(.scrollWheelEventDeltaAxis1, value: -delta)
-  event.setIntegerValueField(.scrollWheelEventFixedPtDeltaAxis1, value: -fixedPtDelta)
+    event.setIntegerValueField(.scrollWheelEventPointDeltaAxis1, value: -pointDelta)
+    event.setIntegerValueField(.scrollWheelEventDeltaAxis1, value: -delta)
+    event.setIntegerValueField(.scrollWheelEventFixedPtDeltaAxis1, value: -fixedPtDelta)
 
-  return Unmanaged.passUnretained(event)
+    return Unmanaged.passUnretained(event)
 }
 
 func configureSignal(signal _signal: Int32, handler: @escaping () -> Void) {
